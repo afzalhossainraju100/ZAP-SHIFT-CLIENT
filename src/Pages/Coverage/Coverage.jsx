cd980f8c0b0a17d101b68f437c0deccc7cf34136
afzalhossainraju100 <raju23105341031@diu.edu.bd>
2026-05-10 13:18:47 +0600

    feat: add map integration with react-leaflet in Coverage component

--- src/Pages/Coverage/Coverage.jsx
+++ src/Pages/Coverage/Coverage.jsx
@@ -1,11 +1,34 @@
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
 
 
 const Coverage = () => {
+  const position = [23.8103, 90.4125];
     return (
       <div>
         <h1>We are available in 64 districts</h1>
         <input type="text" />
         <p>We deliver almost all over Bangladesh</p>
+        {/* alskdfj */}
+        <div className="w-full h-[500px] border-1">
+          <MapContainer 
+          center={position} 
+          zoom={13} 
+          scrollWheelZoom={false}
+          className="h-[500px]"
+          >
+            <TileLayer
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+
+            <Marker position={position}>
+              <Popup>
+                A pretty CSS3 popup. <br /> Easily customizable.
+              </Popup>
+            </Marker>
+          </MapContainer>
+        </div>
       </div>
     );
 };
