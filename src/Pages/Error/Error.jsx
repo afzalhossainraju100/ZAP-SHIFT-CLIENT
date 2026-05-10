20d438d6e0945169573b7715efe69e1a8e66c997
afzalhossainraju100 <raju23105341031@diu.edu.bd>
2026-05-10 22:55:00 +0600

    fix: adjust layout and image size in Error component

--- src/Pages/Error/Error.jsx
+++ src/Pages/Error/Error.jsx
@@ -3,12 +3,15 @@ import { NavLink } from "react-router-dom";
 
 const Error = () => {
     return (
-        <div className="flex flex-col items-center justify-center gap-8 min-h-screen">
-            <img src={errorImg} alt="Error" />
-            <NavLink to="/" className="bg-[#7a8d2b] text-white px-6 py-3 rounded-full hover:bg-[#6a7d1b] transition">
-                Go Back
-            </NavLink>
-        </div>
+      <div className="flex flex-col items-center justify-center gap-8 Auto ">
+        <img width="300" src={errorImg} alt="Error" />
+        <NavLink
+          to="/"
+          className="bg-[#7a8d2b] text-white px-6 py-3 rounded-full hover:bg-[#6a7d1b] transition"
+        >
+          Go Back
+        </NavLink>
+      </div>
     );
 };
 
