c0875a26436a58a8b756b4176d65795cac0af676
afzalhossainraju100 <raju23105341031@diu.edu.bd>
2026-05-18 19:29:45 +0600

    feat: enhance Payment component to include payment handling and display cost

--- src/Pages/Dashboard/Payment/Payment.jsx
+++ src/Pages/Dashboard/Payment/Payment.jsx
@@ -27,11 +27,22 @@ const Payment = () => {
     );
   }
 
+  const handlePayment = async () => {
+    const paymentInfo ={
+        cost: parcel.cost,
+        parcelId: parcel._id,
+        senderEmail: parcel.senderEmail,
+        parcelName: parcel.parcelName,
+    }
+    const res = await axiosSecure.post("/create-checkout-session", paymentInfo);
+    console.log(res.data);
+  };
+
   return (
     <div className="mt-20 mx-4 bg-[#ffffff] text-[#000000] h-screen flex items-center justify-center gap-5">
-      <h1>Please Pay: {parcel?.parcelName ?? "Unknown parcel"}</h1>
+      <h1>Please Pay ${parcel?.cost ?? "0.00"} for: {parcel?.parcelName ?? "Unknown parcel"}</h1>
 
-      <button className="bg-[#caeb66] hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+      <button onClick={handlePayment} className="bg-[#caeb66] hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
         Proceed to Payment
       </button>
     </div>
