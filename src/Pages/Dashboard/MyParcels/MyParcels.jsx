b32ee8bcf109b8fcb0ed8ceececdd5fda457a793
afzalhossainraju100 <raju23105341031@diu.edu.bd>
2026-05-19 19:24:31 +0600

    feat: implement payment handling in MyParcels component with direct payment button

--- src/Pages/Dashboard/MyParcels/MyParcels.jsx
+++ src/Pages/Dashboard/MyParcels/MyParcels.jsx
@@ -5,7 +5,7 @@ import { FiEdit } from "react-icons/fi";
 import { FaMagnifyingGlass } from "react-icons/fa6";
 import { AiOutlineDelete } from "react-icons/ai";
 import Swal from "sweetalert2";
-import { Link } from "react-router-dom";
+
 
 const MyParcels = () => {
   const { user } = useAuth();
@@ -50,6 +50,23 @@ const MyParcels = () => {
     });
   };
 
+  const handlePayment =async (parcel) => {
+    const paymentInfo = {
+      cost: parcel.cost,
+      senderEmail: parcel.senderEmail,
+      parcelId: parcel._id,
+      parcelName: parcel.parcelName,
+    }
+    const res = await axiosSecure.post(
+        "/create-checkout-session",
+        paymentInfo,
+      );
+      if (res.data?.url) {
+        window.location.href = res.data.url;
+      }
+
+  };
+
   return (
     <div className="text-[#000000] mt-20 mx-4 bg-[#ffffff]">
       <h1>All of my parcels: {parcels.length}</h1>
@@ -79,12 +96,18 @@ const MyParcels = () => {
                   {parcel.paymentStatus === "paid" ? (
                     <span className="text-green-400">Paid</span>
                   ) : (
-                    <Link
-                      to={`/dashboard/payment/${parcel._id}`}
+                    // <Link
+                    //   to={`/dashboard/payment/${parcel._id}`}
+                    //   className="btn btn-sm bg-[#caeb66] border-[#caeb66] text-[#000000] hover:bg-[#caeb66] hover:border-[#caeb66]"
+                    // >
+                    //   Pay Now
+                    // </Link>
+                    <button
+                    onClick ={() => handlePayment(parcel)}
                       className="btn btn-sm bg-[#caeb66] border-[#caeb66] text-[#000000] hover:bg-[#caeb66] hover:border-[#caeb66]"
                     >
                       Pay Now
-                    </Link>
+                    </button>
                   )}
                 </td>
                 <td>{parcel.deliveryStatus}</td>
