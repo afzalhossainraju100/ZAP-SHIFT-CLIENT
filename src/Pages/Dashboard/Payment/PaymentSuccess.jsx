14b4bef2c2161e3e78a264d8721c581abca39965
afzalhossainraju100 <raju23105341031@diu.edu.bd>
2026-05-19 21:46:35 +0600

    feat: integrate secure axios instance for payment success handling

--- src/Pages/Dashboard/Payment/PaymentSuccess.jsx
+++ src/Pages/Dashboard/Payment/PaymentSuccess.jsx
@@ -1,5 +1,6 @@
 import { useSearchParams } from "react-router";
 import { useEffect } from "react";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
 
 
 const PaymentSuccess = () => {
@@ -7,12 +8,17 @@ const PaymentSuccess = () => {
     const [searchParams] = useSearchParams();
     const sessionId = searchParams.get("session_id");
     console.log(sessionId);
+    const axiosSecure = useAxiosSecure();
 
     useEffect (() => {
         if (sessionId) {
-            <h1>Payment Successful</h1>
+            axiosSecure
+              .patch(`/payment-success?session_id=${sessionId}`)
+              .then((res) => {
+                console.log(res.data);
+              });
         }
-    }, [sessionId]);
+    }, [sessionId,axiosSecure]);
 
     return (
         <div className="mt-20 mx-4 bg-[#ffffff] text-[#000000] h-screen flex items-center justify-center gap-5">
